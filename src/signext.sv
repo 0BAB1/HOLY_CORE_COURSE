a7d5635bee28f0d03ef0843be1679a1c35be097e
BRH <62248962+0BAB1@users.noreply.github.com>
2024-10-30 14:12:56 +0100

    save

---
+++ src/signext.sv
@@ -0,0 +1,12 @@
+module signext (
+    // IN
+    input logic [24:0] raw_src,
+    input logic [1:0] imm_source,
+
+    // OUT (immediate)
+    output logic [11:0] signed_imm
+);
+
+// todo
+    
+endmodule
--- tb/alu/test_alu.py
+++ tb/alu/test_alu.py
@@ -7,20 +7,40 @@ import random
 @cocotb.test()
 async def alu_test(dut):
     # Start a 10 ns clock
-    cocotb.start_soon(Clock(dut.clk, 1, units="ns").start())
+    cocotb.start_soon(Clock(dut.clk, 10, units="ns").start())
     await RisingEdge(dut.clk)
 
     # TEST ADD
-    # The alu simpply does full ader for add.
-    # The resulting 32 bits can be interpreted as  signed,
+    # The alu simpply does a biwise add.
+    # The resulting 32 bits can be interpreted as signed,
     # unsigned, just like the sources. It all depends on 
     # our interpretation.
 
-    dut.alu_control.value = 0
-    for _ in range(100):
-        # todo
-        pass
-    
+    dut.alu_control.value = 0b000
+    for _ in range(1000):
+        src1 = random.randint(0,0xFFFFFFFF)
+        src2 = random.randint(0,0xFFFFFFFF)
+        dut.src1.value = src1
+        dut.src2.value = src2
+        # We mask expected to not take account of overflows
+        expected = (src1 + src2) & 0xFFFFFFFF
+        # Await 1 ns for the infos to propagate
+        await Timer(1, units="ns")
+        assert int(dut.alu_result.value) == expected
+
     # TEST DEFAULT ALU
+    await RisingEdge(dut.clk)
+    dut.alu_control.value = 0b111
+    src1 = random.randint(0,0xFFFFFFFF)
+    src2 = random.randint(0,0xFFFFFFFF)
+    dut.src1.value = src1
+    dut.src2.value = src2
+    expected = 0
+    # Await 1 ns for the infos to propagate
+    await Timer(1, units="ns")
+    assert int(dut.alu_result.value) == expected
+
 
     # TEST ZERO FLAG
+    assert int(dut.zero.value) == 1
+
