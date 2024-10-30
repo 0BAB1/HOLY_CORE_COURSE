54b1adabbe1dc50274f49c9b4f6c644031c6074a
BRH <hb.hugobabin@gmail.com>
2024-10-30 18:52:59 +0100

    corrected : no clk in alu for tb

--- src/alu.sv
+++ src/alu.sv
@@ -1,8 +1,8 @@
-module moduleName (
+module alu (
     // IN
     input logic [2:0] alu_control,
     input logic [31:0] src1,
-    input logic [31:0] scr2,
+    input logic [31:0] src2,
     // OUT
     output logic [31:0] alu_result,
     output logic zero
@@ -10,7 +10,7 @@ module moduleName (
 
 always_comb begin
     case (alu_control)
-        3'b000 : alu_result = src1 + scr2;
+        3'b000 : alu_result = src1 + src2;
         default: alu_result = 32'b0;
     endcase
 end
