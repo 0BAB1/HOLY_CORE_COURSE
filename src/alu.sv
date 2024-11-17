0722c2b5f14c31f6c1b66f8212baf29e64d20e57
BRH <62248962+0BAB1@users.noreply.github.com>
2024-11-17 10:19:14 +0100

    update alu signals

--- src/alu.sv
+++ src/alu.sv
@@ -11,30 +11,32 @@ module alu (
     output logic last_bit
 );
 
+import holy_core__pkg::*;
+
 wire [4:0] shamt = src2[4:0];
 
 always_comb begin
     case (alu_control)
         // ADD STUFF
-        4'b0000 : alu_result = src1 + src2;
+        ALU_ADD : alu_result = src1 + src2;
         // AND STUFF
-        4'b0010 : alu_result = src1 & src2;
+        ALU_AND : alu_result = src1 & src2;
         // OR STUFF
-        4'b0011 : alu_result = src1 | src2;
+        ALU_OR : alu_result = src1 | src2;
         // SUB Stuff (src1 - src2)
-        4'b0001 : alu_result = src1 + (~src2 + 1'b1);
+        ALU_SUB : alu_result = src1 + (~src2 + 1'b1);
         // LESS THAN COMPARE STUFF (src1 < src2)
-        4'b0101 : alu_result = {31'b0, $signed(src1) < $signed(src2)};
+        ALU_SLT : alu_result = {31'b0, $signed(src1) < $signed(src2)};
         // LESS THAN COMPARE STUFF (src1 < src2) (UNSIGNED VERSION)
-        4'b0111 : alu_result = {31'b0, src1 < src2};
+        ALU_SLTU : alu_result = {31'b0, src1 < src2};
         // XOR STUFF
-        4'b1000 : alu_result = src1 ^ src2;
+        ALU_XOR : alu_result = src1 ^ src2;
         // SLL STUFF
-        4'b0100 : alu_result = src1 << shamt;
+        ALU_SLL : alu_result = src1 << shamt;
         // SRL STUFF
-        4'b0110 : alu_result = src1 >> shamt;
+        ALU_SRL : alu_result = src1 >> shamt;
         // SRA STUFF
-        4'b1001 : alu_result = $signed(src1) >>> shamt;
+        ALU_SRA : alu_result = $signed(src1) >>> shamt;
         default : alu_result = 32'd0;
     endcase
 end
