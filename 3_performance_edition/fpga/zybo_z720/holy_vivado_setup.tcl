abfbd883248feb2c9bc9a3400494e43f36f0e161
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2025-11-22 09:17:03 +0100

    dumped performance edition

--- 3_performance_edition/fpga/zybo_z720/holy_vivado_setup.tcl
+++
@@ -1,5 +0,0 @@
-# todo : re-implement on zybo. mine is corrupted : i gotta get a new one.
-# in the meantime, if you read this, I still havent added back zybo support.
-# BUT you can do it its pretty easy.
-#
-# best, BRH :)
--- 3_performance_edition/vendor/readme.md
+++
@@ -1,5 +0,0 @@
-# External IPs list
-
-This folder contains code from external parties, all in the same folder to ease imports.
-
-Most of these (if not all) come from pulp platform.
