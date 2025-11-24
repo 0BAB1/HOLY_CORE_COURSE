9c84977248baddc49c5b5b964f87686b3a6074b7
0BAB1 <62248962+0BAB1@users.noreply.github.com>
2025-11-24 17:20:24 +0100

    gathered simu perfs, going to run bitstream...

--- 3_perf_edition/fpga/ROM/boot_rom.v
+++ 3_perf_edition/fpga/ROM/boot_rom.v
@@ -5,7 +5,7 @@ module boot_rom(
     output reg [31:0] data_out
 );
 
-    reg [31:0] rom [6:0];
+    reg [31:0] rom [11:0];
 
     initial begin
         rom[0] = 32'h00000293;
@@ -13,8 +13,13 @@ module boot_rom(
         rom[2] = 32'hfff28293;
         rom[3] = 32'h7c129073;
         rom[4] = 32'h7c231073;
-        rom[5] = 32'h800002b7;
-        rom[6] = 32'h00028067;
+        rom[5] = 32'h00000293;
+        rom[6] = 32'h80000337;
+        rom[7] = 32'hfff30313;
+        rom[8] = 32'h7c329073;
+        rom[9] = 32'h7c431073;
+        rom[10] = 32'h800002b7;
+        rom[11] = 32'h00028067;
     end
 
     always @(*) begin
