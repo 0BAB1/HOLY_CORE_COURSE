9cf8de853d00bfd974ffd64a9a392e2b5f097ebe
Sushant Mondal <contact@sushantmondal.com>
2026-07-02 21:01:45 +0530

    revert: 2-space indentation -> 4-space indentation

--- 0_single_cycle_edition/src/regfile.sv
+++ 0_single_cycle_edition/src/regfile.sv
@@ -10,44 +10,44 @@
 
 module regfile (
 
-  // Basic signals
-  input  logic        clk,
-  input  logic        rst_n,
+    // Basic signals
+    input  logic        clk,
+    input  logic        rst_n,
 
-  // Reads
-  input  logic [4:0]  address1,
-  input  logic [4:0]  address2,
-  output logic [31:0] read_data1,
-  output logic [31:0] read_data2,
+    // Reads
+    input  logic [4:0]  address1,
+    input  logic [4:0]  address2,
+    output logic [31:0] read_data1,
+    output logic [31:0] read_data2,
 
-  // Writes
-  input  logic        write_enable,
-  input  logic [31:0] write_data,
-  input  logic [4:0]  address3
+    // Writes
+    input  logic        write_enable,
+    input  logic [31:0] write_data,
+    input  logic [4:0]  address3
 
 );
 
-  // 32 bits register. 32 of them (addressed with 5 bits)
-  reg [31:0] registers [0:31];
+    // 32 bits register. 32 of them (addressed with 5 bits)
+    reg [31:0] registers [0:31];
 
-  // Write logic
-  always @(posedge clk) begin
-    // reset support, init to 0
-    if(rst_n == 1'b0) begin
-      for(int i = 0; i<32; i++) begin
-        registers[i] <= 32'b0;
-      end
+    // Write logic
+    always @(posedge clk) begin
+        // reset support, init to 0
+        if(rst_n == 1'b0) begin
+            for(int i = 0; i<32; i++) begin
+                registers[i] <= 32'b0;
+            end
+        end
+        // Write, except on 0, reserved for a zero constant according to RISC-V specs
+        else if(write_enable == 1'b1 && address3 != 0) begin
+            registers[address3] <= write_data;
+        end
     end
-    // Write, except on 0, reserved for a zero constant according to RISC-V specs
-    else if(write_enable == 1'b1 && address3 != 0) begin
-      registers[address3] <= write_data;
-    end
-  end
 
-  // Read logic, async
-  always_comb begin : read_logic
-    read_data1 = registers[address1];
-    read_data2 = registers[address2];
-  end : read_logic
+    // Read logic, async
+    always_comb begin : read_logic
+        read_data1 = registers[address1];
+        read_data2 = registers[address2];
+    end : read_logic
 
 endmodule : regfile
